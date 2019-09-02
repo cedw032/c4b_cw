6956d3da7f411419b4e4c65f1a6dd11b227cedf2
Chad Edwards <chad@fergusapp.com>
2019-09-03 04:05:39 +1200

    Set production socket

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,8 +1,8 @@
 import {useMemo} from 'react';
 import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:5000');
-//const socket = io.connect();
+//const socket = io.connect('http://localhost:5000');
+const socket = io.connect('https://still-falls-37601.herokuapp.com/');
 
 const useSocket = listeners => useMemo(() => {	
 	Object.entries(listeners).forEach(([name, listener]) => {
